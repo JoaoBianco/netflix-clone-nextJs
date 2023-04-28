1dfcfe48e0eb9ed6da3cb4f2b41b32411b7db28d
JoaoBianco <pepe.bianco@hotmail.com>
2023-04-28 11:20:48 -0300

    Create session and protecting routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-      <h1>Netflix Clone</h1>
+      <h1 className="text-green-700 text-6xl">Netflix Clone</h1>
+      <p className="text-white text-lg">Logged in as {user?.name}</p>
+      <button
+        className="h-10 w-full bg-white font-bold"
+        onClick={() => signOut()}
+      >
+        Logout!
+      </button>
     </>
   );
 }
