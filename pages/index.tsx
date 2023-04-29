cd0c296473c07fc841856b8cdc8799e603e3c991
JoaoBianco <joaops.bianco@gmail.com>
2023-04-29 09:46:07 -0300

    NavBar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import NavBar from "@/components/NavBar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -20,18 +21,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-      <h1 className="text-green-700 text-6xl">Netflix Clone</h1>
-      <p className="text-white text-lg">Logged in as {user?.name}</p>
-      <button
-        className="h-10 w-full bg-white font-bold"
-        onClick={() => signOut()}
-      >
-        Logout!
-      </button>
+      <NavBar />
     </>
   );
 }
