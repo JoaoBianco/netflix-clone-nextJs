b0855c93477cf736f1a045db998f459738173b0a
JoaoBianco <joaops.bianco@gmail.com>
2023-04-29 17:08:57 -0300

    InfoModal and finish project

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface AccountMenuProps {
   visible?: boolean;
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  const { data: user } = useCurrentUser();
+
   if (!visible) {
     return null;
   }
+
   return (
     <div className="bg-black w-56 absolute top-14 right-0 py-5 flex flex-col border-2 border-gray-800">
       <div className="flex flex-col gap-3">
@@ -19,7 +23,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
             alt=""
           />
           <p className="text-white text-sm group-hover/item:underline">
-            Username
+            {user?.name}
           </p>
         </div>
         <hr className="bg-gray-600 border-0 h-px my-4" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import BillBoard from "@/components/BillBoard";
+import InfoModal from "@/components/InfoModal";
 import MovieList from "@/components/MovieList";
 import NavBar from "@/components/NavBar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -27,9 +29,11 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favoriteMovies = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <NavBar />
       <BillBoard />
       <div className="pb-40">
