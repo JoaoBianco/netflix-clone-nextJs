b5e0c3657824ac5da4ffa08d7b4a84bb2eda2d3a
JoaoBianco <joaops.bianco@gmail.com>
2023-04-30 20:46:06 -0300

    Update prismadb connection

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = global.prismadb || new PrismaClient();
-if (process.env.NODE_ENV === "production") global.prismadb = client;
+const client =
+  global.prismadb ||
+  new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } });
+if (process.env.NODE_ENV === "production")
+  global.prismadb = new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL } },
+  });
 
 export default client;
